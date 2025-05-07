2bfff8cf9520c6e78b21c7212f7a78b1835fe500
Harry Nguyen <hungdn210@gmail.com>
2025-05-08 00:48:12 +0100

    add new code 08/05

---
+++ data_repository.py
@@ -0,0 +1,12 @@
+DAYS_WITH_GROUND_FROST = 'groundfrost'  # Number of days with ground frost
+RELATIVE_HUMIDITY_SURFACE = 'hurs'      # Relative humidity at the surface level (%)
+SEA_LEVEL_PRESSURE = 'psl'              # Sea level pressure (hPa)
+POTENTIAL_EVAPOTRANSPIRATION = 'pv'     # Potential evapotranspiration (mm)
+TOTAL_PRECIPITATION = 'rainfall'        # Total precipitation including rain, snow, sleet, hail (mm)
+SURFACE_WIND_SPEED = 'sfcWind'          # Surface wind speed (m/s)
+DAYS_WITH_SNOW_LYING = 'snowLying'      # Number of days with snow lying on the ground
+TOTAL_SUNSHINE_DURATION = 'sun'         # Total sunshine duration (hours)
+NEAR_SURFACE_TEMPERATURE = 'tas'        # Near-surface air temperature (°C)
+CATEGORY_LIST = {
+
+}
